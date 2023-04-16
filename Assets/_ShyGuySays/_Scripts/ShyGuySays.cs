e05e100aff82b46d96203f152f2aec8e85ce4d11
TheKuroEver <mariusztang@outlook.com>
2023-04-16 20:57:35 +0100

    Meta files ?

---
+++ Assets/_ShyGuySays/_Scripts/ShyGuySays.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShyGuySays : MonoBehaviour {
+
+    public KMAudio Audio;
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
