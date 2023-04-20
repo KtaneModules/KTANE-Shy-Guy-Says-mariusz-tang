7af790f251ee958d28f47b0ffa297481100443b7
TheKuroEver <mariusztang@outlook.com>
2023-04-20 11:59:17 +0100

    TP

--- Assets/_ShyGuySays/_Scripts/ShyGuySays.cs
+++ Assets/_ShyGuySays/_Scripts/ShyGuySays.cs
@@ -12,10 +12,12 @@ public class ShyGuySays : MonoBehaviour {
 
     private static int _moduleIdCounter = 0;
     private int _moduleId;
+    private bool TwitchPlaysActive;
 
     [SerializeField] private FlagButton[] _buttonFlags;
     [SerializeField] private ShyGuyButton _centreButton;
     [SerializeField] private FlagController _display;
+    [SerializeField] private KMSelectable _colourblindButton;
 
     private readonly FlagRaise[] _strikeRaises = new FlagRaise[] {
         new FlagRaise(0, Color.red, 'X'),
@@ -63,6 +65,7 @@ public class ShyGuySays : MonoBehaviour {
             }
             strikeMessage += ".";
             Strike(strikeMessage);
+            StopCoroutine(_timer);
             _activeStage = false;
         }
 
@@ -97,6 +100,9 @@ public class ShyGuySays : MonoBehaviour {
         _display.EnqueueLoop(actions, 4f / (_stageNumber * 1 + 1));
 
         int time = 50 + _stageNumber * 10;
+        if (TwitchPlaysActive) {
+            time += 25;
+        }
         _timer = StartCoroutine(StartTimer(time));
         Log("You have " + time + " seconds.");
         _actualInput = string.Empty;
@@ -146,5 +152,71 @@ public class ShyGuySays : MonoBehaviour {
         Module.HandleStrike();
         Log("✕ " + message);
         _display.Enqueue(new FlagAction(1, _strikeRaises), clearExistingActions: true);
+    }
+
+#pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"Use '!{0} <start/shyguy>' to press the Shy Guy button | '!{0} <colorblind/cb>' | "
+        + "'!{0} <L/R>' to press flags. Optionally, chain presses without spaces, eg '!{0} LRLR' | On TP, the timer is extended by 25 seconds.";
+#pragma warning restore 414
+
+    private string _tpButtonNames = "LR";
+
+    private IEnumerator ProcessTwitchCommand(string command) {
+        command = command.Trim().ToUpper();
+
+        if (command == string.Empty) {
+            yield return "sendtochaterror That's an empty command...";
+        }
+        if (command.Length > 11) {
+            yield return "sendtochaterror Your command is too long!";
+        }
+        if (command.Contains(" ")) {
+            yield return "sendtochaterror There should be no spaces in your command.";
+        }
+
+        yield return null;
+
+        if (command == "START" || command == "SHYGUY") {
+            _centreButton.GetComponent<KMSelectable>().OnInteract();
+            yield return new WaitForSeconds(0.2f);
+            _centreButton.GetComponent<KMSelectable>().OnInteractEnded();
+        }
+        else if (command == "COLORBLIND" || command == "CB" || command == "COLOURBLIND") {
+            _colourblindButton.OnInteract();
+        }
+        else if (command.Replace("R", string.Empty).Replace("L", string.Empty) == string.Empty) {
+            foreach (char letter in command) {
+                yield return "multiple strikes"; // Allow TP to release the button before stopping the routine.
+                _buttonFlags[_tpButtonNames.IndexOf(letter)].GetComponent<KMSelectable>().OnInteract();
+                yield return new WaitForSeconds(0.1f);
+                _buttonFlags[_tpButtonNames.IndexOf(letter)].GetComponent<KMSelectable>().OnInteractEnded();
+                yield return "end multiple strikes";
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+        else {
+            yield return "sendtochaterror Invalid command.";
+            yield break;
+        }
+    }
+
+    private IEnumerator TwitchHandleForcedSolve() {
+        yield return null;
+
+        while (_stageNumber < 4) {
+            if (!_activeStage) {
+                _centreButton.GetComponent<KMSelectable>().OnInteract();
+                yield return new WaitForSeconds(0.2f);
+                _centreButton.GetComponent<KMSelectable>().OnInteractEnded();
+            }
+
+            while (_activeStage) {
+                int position = _tpButtonNames.IndexOf(_expectedInput[_actualInput.Length]);
+                _buttonFlags[position].GetComponent<KMSelectable>().OnInteract();
+                yield return new WaitForSeconds(0.1f);
+                _buttonFlags[position].GetComponent<KMSelectable>().OnInteractEnded();
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
     }
 }
